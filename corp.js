8ba2d32cb78a33077072feed328cc478d7a31299
Jenna Korhonen <jenkorho@ulapland.fi>
2021-02-15 18:56:58 +0200

    Uuden sivun jatkoa, pieniä lisäyksiä

--- corp.js
+++ corp.js
@@ -1,2 +1,22 @@
-const lines = document.querySelector(".svg-img").contentDocument;
-console.log(lines);
+const svgDoc = document.querySelector(".svg-img");
+let svgLines;
+
+svgDoc.addEventListener("load", function(){
+    let doc = svgDoc.getSVGDocument();
+    console.log(doc);
+    let lines = doc.getElementsByTagName("line");
+    svgLines = lines;
+    console.log(svgLines);
+    for(l of svgLines) {
+        l.classList.add("svg-line");
+    }
+    console.log(svgLines);
+
+});
+
+
+const tl = new TimelineMax();
+function animateLines() {
+
+
+}
