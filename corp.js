d69d388602b691fd6a45d9f2ddce878b4d8637fa
Jenna Korhonen <jenkorho@ulapland.fi>
2021-02-15 23:36:45 +0200

    Lisäilyä uusille sivuille, vanhaan linkit

--- corp.js
+++ corp.js
@@ -2,6 +2,7 @@ const svgDoc = document.querySelector(".svg-img");
 let svgLines;
 
 svgDoc.addEventListener("load", function(){
+
     let doc = svgDoc.getSVGDocument();
     console.log(doc);
     let lines = doc.getElementsByTagName("line");
@@ -11,12 +12,65 @@ svgDoc.addEventListener("load", function(){
         l.classList.add("svg-line");
     }
     console.log(svgLines);
+/*     animateLines(); */
+
+});
+
+//Underline
+
+$(".nav-link").mouseover((event) => {
+
+    let element = event.currentTarget;
+    //let underline = element.firstChild;
+    let underline = element.lastChild.previousElementSibling;
+    console.log(underline);
+    underline.classList.toggle("anim");
+
+});
+
+$("#message-btn").mouseover((event) => {
+
+    let element = event.currentTarget;
+    //let underline = element.firstChild;
+    let underline = element.nextSibling.nextSibling;
+    console.log(underline);
+    underline.classList.toggle("anim");
+
+});
+
+$("#message-btn").mouseout((event) => {
+
+    let element = event.currentTarget;
+    //let underline = element.firstChild;
+    let underline = element.nextSibling.nextSibling;
+    console.log(element);
+    underline.classList.toggle("anim");
+
+});
+
+$(".nav-link").mouseout((event) => {
+
+    let element = event.currentTarget;
+    let underline = element.lastChild.previousElementSibling;
+    console.log(underline);
+    underline.classList.toggle("anim");
 
 });
 
 
-const tl = new TimelineMax();
+
+
+/* 
+
 function animateLines() {
+    const tl = new TimelineMax();
+    for(l of svgLines) {
+        let e = l;
+        let c = e.getAttribute("class");
+        tl.from(("." + c), 1, {scaleX: 0, transformOrigin: "left", ease: Power2.easeIn} );
+        console.log(svgLines);
+    };
+    
 
-
-}
+};
+ */
